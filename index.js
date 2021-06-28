76c55b5e77b774291a2c08d3ef17298bdc123895
akibmak <1.makakib@gmail.com>
2021-06-28 16:24:38 +0530

    add new card and local storage

---
+++ index.js
@@ -0,0 +1,84 @@
+//this will get elment s in array format
+// const taskContainer = document.getElementsByClassName("task_container");
+// console.log(taskContainer);
+
+//to get the elements directly we can usr querySelector.
+const taskContainer  = document.querySelector(".task_container");
+
+const globalStore = [];
+
+const generateNewCard = (taskData) => `
+<div class="col-md-6 col-lg-4" id=${taskData.id}>
+      <div class="card">
+        <div class="card-header d-flex justify-content-end gap-2">
+          <button type="button" class="btn btn-success"><i class="fas fa-pencil-alt"></i></button>
+          <button type="button" class="btn btn-danger"><i class="fas fa-trash-alt"></i></button>
+        </div>
+        <img
+          src=${taskData.imageUrl}
+          class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${taskData.taskTitle}</h5>
+          <p class="card-text">${taskData.taskDescryption}</p>
+          <a href="#" class="btn btn-primary">${taskData.taskType}</a>
+        </div>
+        <div class="card-footer  ">
+            <button type="button" class="btn btn-outline-primary float-end">Open Task</button>
+        </div>
+      </div>
+    </div> 
+    `;
+
+const loadInitialCardData = () => {
+    // localstorage to get tasky card data
+    const getCardData = localStorage.getItem("tasky");
+
+    // convert from string to normal object
+    const {cards} = JSON.parse(getCardData);
+
+    // loop over those array of task object to create HTML card , inject it to DOM
+    cards.map((cardObject) => {
+        taskContainer.insertAdjacentHTML("beforeend", generateNewCard(cardObject));
+
+         //adding user input task data in array 
+         globalStore.push(cardObject);
+    })
+}
+
+const saveChanges = () => {
+    const taskData = {
+        id: `${Date.now()}`, //unique number of id . We used $ to include javaScript expression inside a string which is enclosed in ``.
+        imageUrl: document.getElementById("imageurl").value,
+        taskTitle: document.getElementById("tasktitle").value,
+        taskType: document.getElementById("tasktype").value,
+        taskDescryption: document.getElementById("taskdescription").value,
+    };
+
+    //this will dynamically capture the data from the taskData and merge it with
+    //card html and will create a new card with user input data
+    
+
+    //how do we add this new card to Dom for that we use insertAdjacent
+   // now we have four options to add this new card to taskContainer 1.before begin, 2.after begin, 3.before end, 4. after end.
+    taskContainer.insertAdjacentHTML("beforeend", generateNewCard(taskData));
+
+    //adding user input task data in array 
+    globalStore.push(taskData);
+
+    //storing the array data in our system(local storage). If we don't use an array and
+    //directly pass the taskData then only the current card data will be stored as it will override previous card data.
+   
+    // this will store data in object form we do not want that .
+  //  localStorage.setItem("tasky",globalStore);
+
+    localStorage.setItem("tasky",JSON.stringify({cards:globalStore}));  //json.stringify expects an object thats why we added cards object.
+};
+
+//Parent object 
+//Browser -> window in browser you can send notification,alerts , open new tab and close new tab etc.
+//DOM -> document  with DOM you can edit the html contents
+
+//local storage <= 5mb
+//API -> Aplication Programming Interface
+
+
