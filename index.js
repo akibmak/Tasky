2bbff462f165c77c577e0574104e277c76cb8d55
akibmak <1.makakib@gmail.com>
2021-06-28 17:51:57 +0530

    add delete cards feature

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-//this will get elment s in array format
-// const taskContainer = document.getElementsByClassName("task_container");
-// console.log(taskContainer);
+/* this will get elment s in array format
+ const taskContainer = document.getElementsByClassName("task_container");
+ console.log(taskContainer); */
 
 //to get the elements directly we can usr querySelector.
 const taskContainer  = document.querySelector(".task_container");
 
-const globalStore = [];
+let globalStore = [];
 
 const generateNewCard = (taskData) => `
-<div class="col-md-6 col-lg-4" id=${taskData.id}>
+<div class="col-md-6 col-lg-4">
       <div class="card">
         <div class="card-header d-flex justify-content-end gap-2">
           <button type="button" class="btn btn-success"><i class="fas fa-pencil-alt"></i></button>
-          <button type="button" class="btn btn-danger"><i class="fas fa-trash-alt"></i></button>
+          <button type="button" class="btn btn-danger" id=${taskData.id} onclick="deleteCard.apply(this, arguments)">
+            <i class="fas fa-trash-alt" id=${taskData.id} onclick="deleteCard.apply(this, arguments)"></i></button>
         </div>
         <img
           src=${taskData.imageUrl}
@@ -73,6 +74,28 @@ const saveChanges = () => {
 
     localStorage.setItem("tasky",JSON.stringify({cards:globalStore}));  //json.stringify expects an object thats why we added cards object.
 };
+
+const deleteCard = (event) => {
+    //we'll ask parent to provide us with the required id
+    event = window.event;
+    const targetID  =  event.target.id;
+
+    const tagName = event.target.tagName; // BUTTON
+
+    //match the id of the element with the id inside globalstore
+    //if match found remmove
+
+    globalStore = globalStore.filter((cardObject) => cardObject.id !== targetID); 
+    localStorage.setItem("tasky",JSON.stringify({cards:globalStore}));
+    // we have updated array of cards
+
+    //contact parent 
+    if(tagName === "BUTTON"){
+      return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode);
+    }else{
+      return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode.parentNode);
+    }
+}
 
 //Parent object 
 //Browser -> window in browser you can send notification,alerts , open new tab and close new tab etc.
